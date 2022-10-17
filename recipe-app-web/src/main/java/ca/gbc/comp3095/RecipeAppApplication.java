2269198cb5114e2046bacc9406923d393e3c4924
nguyenlean96 <nguyenlean96@gmail.com>
2022-10-17 15:54:09 -0400

    code as of 4pm, oct 17th

--- recipe-app-web/src/main/java/ca/gbc/comp3095/RecipeAppApplication.java
+++ recipe-app-web/src/main/java/ca/gbc/comp3095/RecipeAppApplication.java
@@ -2,15 +2,15 @@ package ca.gbc.comp3095;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RecipeAppApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(RecipeAppApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(RecipeAppApplication.class, args);
 		System.out.println("Application started...");
-
 	}
 
 }
--- recipe-app-web/src/main/java/ca/gbc/comp3095/controllers/UserAccessController.java
+++ recipe-app-web/src/main/java/ca/gbc/comp3095/controllers/UserAccessController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping({"/", ""})
+@RequestMapping({"/account", "account"})
 public class UserAccessController {
 
     @RequestMapping("/login")
@@ -17,6 +17,32 @@ public class UserAccessController {
         System.out.println("Loging in access...");
 
         mv.setViewName("/users/uac/login");
+        return mv;
+    }
+
+    @RequestMapping("/logout")
+    public ModelAndView logout(Model model) {
+        ModelAndView mv = new ModelAndView();
+
+
+        mv.setViewName("/users/uac/logout");
+        return mv;
+    }
+
+    @RequestMapping("/account/profile")
+    public ModelAndView view(Model model) {
+        ModelAndView mv = new ModelAndView();
+
+
+        mv.setViewName("/users/profile");
+        return mv;
+    }
+
+    @RequestMapping("/account/edit")
+    public ModelAndView edit(Model model) {
+        ModelAndView mv = new ModelAndView();
+
+        mv.setViewName("/users/user-edit");
         return mv;
     }
 }
