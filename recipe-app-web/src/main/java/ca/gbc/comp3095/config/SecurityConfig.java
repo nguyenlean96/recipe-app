49c23bf01f47fe161b3af15121895aa4e6e08a95
nguyenlean96 <nguyenlean96@gmail.com>
2022-10-21 18:01:54 -0400

    Security layer added and disabled for continuing other functions testing

--- recipe-app-web/src/main/java/ca/gbc/comp3095/config/SecurityConfig.java
+++ recipe-app-web/src/main/java/ca/gbc/comp3095/config/SecurityConfig.java
@@ -1,2 +1,21 @@
-package ca.gbc.comp3095.config;public class SecurityConfig {
+package ca.gbc.comp3095.config;
+/*
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+    }
 }
+
+ */
