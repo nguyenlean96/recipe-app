1ef0f0507854d6b1f012e20fb8840daeeec838d2
Sarah Sami <sarah@Sarahs-MacBook-Pro.local>
2022-10-23 14:26:27 -0400

    CookbookController and MyRecipeController added

---
+++ recipe-app-web/src/main/java/ca/gbc/comp3095/controllers/CookbookController.java
@@ -0,0 +1,5 @@
+package ca.gbc.comp3095.controllers;
+
+public class CookbookController {
+    // to controll user recipe table rows where IsSaved is 1 ----- if a user saves a recipe, it will be added to the user recipe table and IsSaved will be set to 1
+}
---
+++ recipe-app-web/src/main/java/ca/gbc/comp3095/controllers/MyRecipeController.java
@@ -0,0 +1,5 @@
+package ca.gbc.comp3095.controllers;
+
+public class MyRecipeController {
+    // to controll user recipe table rows where IsCreated is 1 ----- if a user creates a recipe, it will be added to the user recipe table and IsCreated will be set to 1
+}
